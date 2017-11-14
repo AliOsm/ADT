01dceca3e857461d595beec52a8a69961eb1798a
IbraheemTuffaha <33587724+IbraheemTuffaha@users.noreply.github.com>
2017-11-14 15:07:27 +0200

    Create lcm.py

---
+++ Math/Least Common Multiple/lcm.py
@@ -0,0 +1,11 @@
+def gcd(a, b): # Greatest Common Divisor
+  if b == 0:
+    return a;
+  
+  return gcd(b, a % b);
+
+def lcm(a, b):
+  return a // gcd(a, b) * b;
+
+a, b = map(int, input().split());
+print(lcm(a, b));
